46ff6d86951b88ab4adaef10e525297ff3a72bf2
Lakbiri <lakbiri.mohamed@gmail.com>
2017-12-21 18:32:28 +0000

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,24 @@
+var $item = $('.carousel-inner .carousel-item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel-inner img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel-inner').carousel({
+  interval: 5000,
+});
